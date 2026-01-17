a4f5a1e1785a906b4695c740e48fc44cf4a9ad89
yuh404 <vanhuy0202@gmail.com>
2026-01-17 23:18:16 +0700

    Xóa thư mục STM32 khỏi GitHub

--- STM32/NVIC.h
+++
@@ -1,8 +0,0 @@
-#ifndef __NVIC_H
-#define __NVIC_H
-#include "stm32f10x.h"                  // Device header
-
-void NVIC_Config_USART1(uint8_t PreemptPriority, uint8_t SubPriority);
-void NVIC_Config_TIM2(uint8_t PreemptPriority, uint8_t SubPriority);
-
-#endif
